98ccf7d46ae51624cb8a4dbdbd8d0e295b83f6e0
João <joaoaugusto311016@gmail.com>
2023-07-20 15:22:58 -0300

    20-07

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,7 +11,7 @@ export default function Home(){
               <li className=" rounded text-white p-2 bg-color-sidebar border-[1px] border-color-border ">Linkedin</li>
               <li className=" rounded text-white flex gap-2 items-center p-2 bg-color-sidebar border-[1px] border-color-border"> <span> <IconEmail /></span> Email</li>
             </ul>
-            <p className=" mb-2 mt-5 font-light text-color-font-principal">Hello! I m João Augusto, a developer passionate about prototyping interactive and minimalist interfaces. Since I entered the world of programming, I have found immense pleasure in combining elegant aesthetics with fluid operation, creating unique experiences for users.</p>
+            <p className=" mb-2 mt-10 font-light text-color-font-principal">Hello! I m João Augusto, a developer passionate about prototyping interactive and minimalist interfaces. Since I entered the world of programming, I have found immense pleasure in combining elegant aesthetics with fluid operation, creating unique experiences for users.</p>
             <p className=" mb-2 mt-5 font-light text-color-font-principal">Hello! I m João Augusto, a developer passionate about prototyping interactive and minimalist interfaces. Since I entered the world of programming, I have found immense pleasure in combining elegant aesthetics with fluid operation, creating unique experiences for users.</p>
             <p className=" mb-2 mt-5 font-light text-color-font-principal">Hello! I m João Augusto, a developer passionate about prototyping interactive and minimalist interfaces. Since I entered the world of programming, I have found immense pleasure in combining elegant aesthetics with fluid operation, creating unique experiences for users.</p>
       </div>
