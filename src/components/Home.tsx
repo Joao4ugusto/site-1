8caf02651164f66df47887f6f4d59f242878211c
João <joaoaugusto311016@gmail.com>
2023-07-20 14:02:53 -0300

    20-07

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,12 +5,11 @@ export default function Home(){
   return(
     <section className=" w-full h-full m-5 mt-32">
       <div className="">
-            <p className=" mb-2 font-medium text-sm text-color-font-secundaria">#AboutMe</p>
-            <h1 className=" text-white text-lg font-bold mb-2">MY NAME IS JOÃO AUGUSTO</h1>
-            <p className=" mb-6 text-base font-medium text-white">I m a Brazilian software developer, born in 2002, I like to prototype interfaces, but I m not a designer haha.</p>
+            <p className=" mb-2 text-lg font-medium text-white">#AboutMe</p>
+            <p className=" mb-6 text-base font-medium text-color-font-principal">My name is João Augusto, I m a Brazilian software developer, born in 2002, I like to prototype interfaces, but I m not a designer haha.</p>
             <ul className=" flex gap-6">
-              <li className=" p-2 bg-color-sidebar border-2 border-color-border">Linkedin</li>
-              <li className=" flex gap-2 items-center p-2 bg-color-sidebar border-2 border-color-border"> <span> <IconEmail /></span> Email</li>
+              <li className=" text-color-font-principal p-2 bg-color-sidebar border-2 border-color-border">Linkedin</li>
+              <li className=" text-color-font-principal flex gap-2 items-center p-2 bg-color-sidebar border-2 border-color-border"> <span> <IconEmail /></span> Email</li>
             </ul>
       </div>
 
