b651404567aeca292302799ab07ea8531fcb1c37
João <joaoaugusto311016@gmail.com>
2023-07-20 14:15:51 -0300

    20-07

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,7 @@ export default function Home(){
             <p className=" mb-2 text-xl font-bold text-white">#AboutMe</p>
             <p className=" mb-6 text-base font-normal text-color-font-principal">My name is João Augusto, I m a Brazilian software developer, born in 2002, I like to prototype interfaces, but I m not a designer haha.</p>
             <ul className=" flex gap-6">
-              <li className=" rounded text-color-font-principal p-2 bg-color-sidebar border-2 border-color-border">Linkedin</li>
+              <li className=" rounded text-color-font-principal p-2 bg-color-border border-2 border-color-border">Linkedin</li>
               <li className=" rounded text-color-font-principal flex gap-2 items-center p-2 bg-color-sidebar border-2 border-color-border"> <span> <IconEmail /></span> Email</li>
             </ul>
       </div>
