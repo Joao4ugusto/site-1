9f2a6baf35299ef9072919963b669a4d7c13be90
João <joaoaugusto311016@gmail.com>
2023-07-20 16:17:32 -0300

    20-07

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,8 +8,8 @@ export default function Home(){
             <p className=" mb-2 text-3xl font-bold text-white">#AboutMe</p>
             <p className=" mb-8 text-base font-ligh text-white">I m a Brazilian software developer, born in 2002, I like to prototype interfaces, but I m not a designer haha.</p>
             <ul className=" flex gap-6">
-              <li className=" rounded text-white p-2 bg-color-sidebar border-[1px] border-color-border ">Linkedin</li>
-              <li className=" rounded text-white flex gap-2 items-center p-2 bg-color-sidebar border-[1px] border-color-border"> <span> <IconEmail /></span> Email</li>
+              <li className=" rounded text-white px-4 py-2 bg-color-sidebar border-[1px] border-color-border ">Linkedin</li>
+              <li className=" rounded text-white flex gap-2 items-center px-4 py-2 bg-color-sidebar border-[1px] border-color-border"> <span> <IconEmail /></span> Email</li>
             </ul>
             <p className=" mb-2 mt-10 font-light text-color-font-principal">Hello! I m João Augusto, a developer passionate about prototyping interactive and minimalist interfaces. Since I entered the world of programming, I have found immense pleasure in combining elegant aesthetics with fluid operation, creating unique experiences for users.</p>
             <p className=" mb-2 mt-5 font-light text-color-font-principal">Hello! I m João Augusto, a developer passionate about prototyping interactive and minimalist interfaces. Since I entered the world of programming, I have found immense pleasure in combining elegant aesthetics with fluid operation, creating unique experiences for users.</p>
@@ -17,6 +17,11 @@ export default function Home(){
             <div className=" rounded border-[1px] border-color-border w-full h-60">
                 <div className=" p-5 border-b-[1px] border-color-border">
                   <h1>Experience</h1>
+                </div>
+                <div className=" w-full h-32 border-b-[1px] border-color-border">
+                    <div>
+
+                    </div>
                 </div>
             </div>
       </div>
