7f5ed05f760bf3db1635e16d4a92d66d5319fedb
João <joaoaugusto311016@gmail.com>
2023-07-20 11:55:59 -0300

    20-07

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,8 +5,8 @@ export default function Home(){
     <section className=" w-full h-full m-4  mt-28">
       <div className="">
             <p className=" mb-2 text-sm text-color-font-secundaria">#AboutMe</p>
-            <h1 className=" mb-2">MY NAME IS JOÃO AUGUSTO</h1>
-            <p className="text-sm text-color-font-secundaria">I'm a Brazilian software developer, born in 2002, I like to prototype interfaces, but I'm not a designer haha</p>
+            <h1 className=" text-white mb-2">MY NAME IS JOÃO AUGUSTO</h1>
+            <p className="text-sm text-color-font-secundaria">I m a Brazilian software developer, born in 2002, I like to prototype interfaces, but I m not a designer haha</p>
       </div>
 
     </section>
