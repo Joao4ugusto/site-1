8fef4be14b019356ddd45cd6d8ea2aaa524fd99c
João <joaoaugusto311016@gmail.com>
2023-07-27 15:54:06 -0300

    27-07

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,8 +10,7 @@ export default function Header({ openSaidebarClick }: props){
     <header className=" z-40 md:hidden fixed  bg-color-black  w-full">
     <div className=" h-[45px] my-4 mx-4 flex justify-between items-center">
     <div>
-      {/* <h1 className=" font-bold text-lg  text-color-font-roxo">J4</h1> */}
-      <IconLogo />
+      <h1 className=" font-bold text-lg text-color-font-roxo">J4</h1>
     </div>
     <div onClick={openSaidebarClick} className=" p-2.5 bg-color-fundo rounded">
       <IconMenu />
