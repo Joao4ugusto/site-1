58b7cc3e317184501b890d654f4700104e97a77d
João <joaoaugusto311016@gmail.com>
2023-07-27 01:26:47 -0300

    27-07

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ type props = {
 
 export default function Header({ openSaidebarClick }: props){
   return(
-    <header className=" z-40 md:hidden fixed bg-color-black w-full">
+    <header className=" z-40 md:hidden fixed  bg-black  w-full">
     <div className=" h-[45px] my-4 mx-4 flex justify-between items-center">
     <div>
       <h1 className=" font-bold text-lg  text-color-font-roxo">J4</h1>
