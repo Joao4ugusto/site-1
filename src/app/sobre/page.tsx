f03e1a3463edf3f73b4e98c2fad603d5249d7979
João <joaoaugusto311016@gmail.com>
2023-07-27 01:17:50 -0300

    27-07

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -9,7 +9,7 @@ export default function page(){
     <>
     <section className=" w-full h-full m-5 mt-32 md:mt-24 ">
       <div className="  md:ml-56 mf:ml-[235px] lg:ml-[450px] lg:w-[600px]">
-            <p className=" mb-4 text-4xl font-bold text-white">About Me</p>
+            <p className=" mb-4 text-[42px] font-bold text-white">About Me</p>
             <p className=" mb-8 text-base font-normal text-white">I m a Brazilian software developer, born in 2002, I like to prototype interfaces, but I m not a designer haha.</p>
             <div className=" flex gap-6">
               <a href="https://www.linkedin.com/in/jo%C3%A3o-augusto-oliveira-15b018238/" className=" hover:bg-color-sidebar/60  cursor-pointer rounded text-white px-5 py-2 bg-color-sidebar border-[1px] border-color-border ">Linkedin</a>
