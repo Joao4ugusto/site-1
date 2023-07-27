6e7aec49c9c02f66b1a2d910c1a3f2b838a1b6f4
João <joaoaugusto311016@gmail.com>
2023-07-27 14:44:26 -0300

    27-07

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header({ openSaidebarClick }: props){
       <h1 className=" font-bold text-lg  text-color-font-roxo">J4</h1>
       {/* <Image src={Logo} alt="Logo menor" /> */}
     </div>
-    <div onClick={openSaidebarClick} className=" p-2 bg-color-border rounded border-[0.5px] border-color-border">
+    <div onClick={openSaidebarClick} className=" p-2 bg-color-border rounded ">
       <IconMenu />
     </div>
     </div>
