3c9b520feaf6c13c9f3ad7ef24bef41fe819ea5c
João <joaoaugusto311016@gmail.com>
2023-07-28 23:30:48 -0300

    28-07

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,22 +7,22 @@ export default function page(){
 
   return(
     <>
-    <section className=" w-full h-full m-5 mt-32 md:mt-20 ">
+    <section className=" w-full h-full m-5 mt-28 md:mt-20 ">
       <div className="  md:ml-56 mf:ml-[235px] lg:ml-[450px] lg:w-[600px]">
-            <p className=" mb-3 text-color-font-secundaria md:hidden">Intro</p>
+            <p className=" text-base md:hidden">Intro</p>
             <p className=" mb-4 text-[35px] font-bold text-white">Developer and student</p>
             <p className=" mb-8 text-base font-normal text-white">Pleasure, I m João Augusto, developer and computer science student, passionate about technology, challenges and learning.</p>
             <div className=" flex gap-4">
               <a href="/sobre" className="  hover:bg-color-fundo/80  cursor-pointer rounded-md text-white px-5 py-2 bg-color-fundo border-[1px] border-color-border ">About Me</a>
               <a href="mailto:joaoaugusto311016@gmail.com" className="  hover:bg-color-fundo/80 cursor-pointer rounded-md text-white flex gap-2 items-center px-5 py-2 bg-color-fundo border-[1px] border-color-border"> <span> <IconEmail /></span> Email</a>
             </div>
-            <div className=" mt-10">
-                <h1 className=" md:text-base font-medium">Blog</h1>
-                <p className=" text-color-font-secundaria text-sm md:text-base mt-5">Check out my articles on dev.to! Short readings with space for your feedback. I sincerely appreciate your opinions</p>
+            <div className=" mt-12">
+                <h1 className=" font-medium text-lg">Blog</h1>
+                <p className=" text-color-font-secundaria text-base mt-5">Check out my articles on dev.to! Short readings with space for your feedback. I sincerely appreciate your opinions</p>
             </div>
             <div className="p-5 mt-10 rounded border-[1px]  border-color-border w-full">
               <h1>Experience</h1>
-              <p className=" text-color-font-secundaria text-sm md:text-base mt-5">Check here my latest professional experiences</p>
+              <p className=" text-color-font-secundaria mt-5">Check here my latest professional experiences</p>
               <div className=" mt-5 flex gap-4">
                      <div className=" text-white font-bold p-3 bg-color-sidebar border-[1px] border-color-border rounded ">
                         <IconAtom  />
@@ -41,7 +41,7 @@ export default function page(){
                         <h3 className="text-color-font-terceira text-sm font-light">Young apprentice</h3>
                      </div> 
               </div>
-              <a className=" hover:bg-color-fundo/80 cursor-pointer text-center font-medium mt-5 block p-2 rounded-md w-full bg-color-fundo border-[1px] border-color-border">view all</a>
+              <a href="/sobre" className=" hover:bg-color-fundo/80 cursor-pointer text-center font-medium mt-5 block p-2 rounded-md w-full bg-color-fundo border-[1px] border-color-border">view all</a>
             </div>
       </div>
       <div className=" right-0 mr-5 rounded-md mb-5 fixed mt-5  bg-white text-black  bottom-0">
