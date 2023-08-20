c724e671bbc53013cc20015521732213deb345c2
João <joaoaugusto311016@gmail.com>
2023-08-20 20:20:08 -0300

    20-08

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
     <div className="  px-6 mt-28 md:px-36 lg:px-40 lg:mt-36 xl:px-[400px] 2xl:mt-40 2xl:px-[450px]">
       <h1 className="text-white mb-8 text-base font-bold">João Augusto</h1>
       <p className=" text-color-font-secundaria  mb-10 font-normal text-base leading-6">I m a Brazilian software engineer, in addition to being a computer science student. My main area of ​​focus is static software development, with an emphasis on creating compelling and scalable solutions.</p>
-      <p className=" text-color-font-secundaria  mb-10 font-normal text-base leading-6">I m currently working at Raycast as a DX Engineer to grow its developer community.</p>
-      <p className=" text-color-font-secundaria  mb-14 font-normal text-base leading-6">Previously, I worked at Rainbow, where I focused on RainbowKit. Before that, I co-created Radix, designed the Stitches API and led their developer community.</p>
+      <p className=" text-color-font-secundaria  mb-10 font-normal text-base leading-6">Currently, I am committed to a continuous study of the principles of data structures and algorithms. If you re interested, you can follow along with my <a className=" underline underline-offset-4 hover:text-color-font-secundaria/60" href="https://github.com/Joao4ugusto/Data-Structure-and-Algorithm">repository</a>.</p>
+      <p className=" text-color-font-secundaria  mb-14 font-normal text-base leading-6">Sometimes I write articles covering various topics in the field of technology on <a className=" underline underline-offset-4 hover:text-color-font-secundaria/60" href="https://dev.to/joao31oliveira">dev.to</a>.</p>
     </div>
     
 
