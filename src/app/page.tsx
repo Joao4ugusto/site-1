0c3e2e9f34bc0f978918b480945e6f9446a5303a
João <joaoaugusto311016@gmail.com>
2023-08-20 04:17:12 -0300

    20-08

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,13 +67,13 @@ export default function Page() {
     </div>
     </header>
 
-    <div className=" px-6 mt-28 md:px-36 lg:px-40 lg:mt-36 xl:px-[400px] 2xl:px-[450px]">
+    <div className=" z-10 px-6 mt-28 md:px-36 lg:px-40 lg:mt-36 xl:px-[400px] 2xl:px-[450px]">
       <h1 className="text-white mb-8 text-base font-bold">João Augusto</h1>
       <p className=" text-color-font-principal  mb-10 font-medium text-base leading-6">I m a UI developer based in Barcelona. I m interested in dev rel, design systems, web3, user/dev experience and under engineering.</p>
       <p className=" text-color-font-principal  mb-10 font-medium text-base leading-6">I m currently working at Raycast as a DX Engineer to grow its developer community.</p>
       <p className=" text-color-font-principal  mb-14 font-medium text-base leading-6">Previously, I worked at Rainbow, where I focused on RainbowKit. Before that, I co-created Radix, designed the Stitches API and led their developer community.</p>
     </div>
-    <div className=" px-6 mt-28 md:px-36 lg:px-40 lg:mt-36 xl:px-[400px] 2xl:px-[450px]">
+    <div className=" z-10 px-6 mt-28 md:px-36 lg:px-40 lg:mt-36 xl:px-[400px] 2xl:px-[450px]">
       <h2>Writing</h2>
     </div>
     
