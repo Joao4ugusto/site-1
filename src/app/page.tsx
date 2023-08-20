1fa5c4a15338b5d9a578c404c9b1fd4650aa7aaa
João <joaoaugusto311016@gmail.com>
2023-08-20 01:02:12 -0300

    20-08

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Page() {
 
     <div className=" px-6 mt-28 md:px-36 lg:px-40 lg:mt-36 xl:px-[400px] 2xl:px-[450px]">
       <h1 className="text-color-font-principal mb-8 text-base font-bold">João Augusto</h1>
-      <p className=" text-color-font-secundaria  mb-10 font-semibold text-base leading-5">I'm a UI developer based in Barcelona. I'm interested in dev rel, design systems, web3, user/dev experience and under engineering.</p>
-      <p className=" text-color-font-secundaria  mb-10 font-semibold text-base leading-5">I'm currently working at Raycast as a DX Engineer to grow its developer community.</p>
+      <p className=" text-color-font-secundaria  mb-10 font-semibold text-base leading-5">I m a UI developer based in Barcelona. I m interested in dev rel, design systems, web3, user/dev experience and under engineering.</p>
+      <p className=" text-color-font-secundaria  mb-10 font-semibold text-base leading-5">I m currently working at Raycast as a DX Engineer to grow its developer community.</p>
       <p className=" text-color-font-secundaria  mb-10 font-semibold text-base leading-5">Previously, I worked at Rainbow, where I focused on RainbowKit. Before that, I co-created Radix, designed the Stitches API and led their developer community.</p>
     </div>
     
